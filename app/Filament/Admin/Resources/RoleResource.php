2378076713028664eac513cb24f53732b9337cfa
MauricioCorreaM <mauriciocorreamolina90@gmail.com>
2026-02-12 23:41:38 -0500

    feat(shield): per-section select all for role permissions

--- app/Filament/Admin/Resources/RoleResource.php
+++ app/Filament/Admin/Resources/RoleResource.php
@@ -3,6 +3,11 @@
 namespace App\Filament\Admin\Resources;
 
 use BezhanSalleh\FilamentShield\Resources\RoleResource as BaseRoleResource;
+use BezhanSalleh\FilamentShield\Facades\FilamentShield;
+use BezhanSalleh\FilamentShield\Support\Utils;
+use Filament\Forms;
+use Filament\Forms\Components\Component;
+use Illuminate\Support\HtmlString;
 use Spatie\Permission\Models\Role;
 
 class RoleResource extends BaseRoleResource
@@ -18,5 +23,48 @@ class RoleResource extends BaseRoleResource
     public static function getModel(): string
     {
         return Role::class;
+    }
+
+    public static function getResourceEntitiesSchema(): ?array
+    {
+        return collect(FilamentShield::getResources())
+            ->sortKeys()
+            ->map(function (array $entity): Forms\Components\Section {
+                $sectionLabel = strval(
+                    static::shield()->hasLocalizedPermissionLabels()
+                        ? FilamentShield::getLocalizedResourceLabel($entity['fqcn'])
+                        : $entity['model']
+                );
+
+                $permissionsArray = static::getResourcePermissionOptions($entity);
+                $selectAllFieldName = static::getSelectAllFieldNameForEntity($entity['resource']);
+
+                $checkboxList = static::getCheckBoxListComponentForResource($entity)
+                    ->afterStateUpdated(function ($state, Forms\Set $set) use ($permissionsArray, $selectAllFieldName): void {
+                        $set($selectAllFieldName, count($state ?? []) === count($permissionsArray));
+                    });
+
+                return Forms\Components\Section::make($sectionLabel)
+                    ->description(fn () => new HtmlString('<span style="word-break: break-word;">' . Utils::showModelPath($entity['fqcn']) . '</span>'))
+                    ->compact()
+                    ->schema([
+                        Forms\Components\Checkbox::make($selectAllFieldName)
+                            ->label(__('filament-shield::filament-shield.field.select_all.name'))
+                            ->dehydrated(false)
+                            ->live()
+                            ->afterStateUpdated(function (bool $state, Forms\Set $set) use ($permissionsArray, $entity): void {
+                                $set($entity['resource'], $state ? array_keys($permissionsArray) : []);
+                            }),
+                        $checkboxList,
+                    ])
+                    ->columnSpan(static::shield()->getSectionColumnSpan())
+                    ->collapsible();
+            })
+            ->toArray();
+    }
+
+    protected static function getSelectAllFieldNameForEntity(string $entityResourceName): string
+    {
+        return '__select_all__' . $entityResourceName;
     }
 }
